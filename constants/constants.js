1211615b7ca86d7ba254b09f41aa00e2d9b4bea1
Nico Blasutig <nicolasblasutig@gmail.com>
2022-05-10 16:13:35 -0300

    first commit

---
+++ constants/constants.js
@@ -0,0 +1,46 @@
+export const projects = [
+  {
+    title: 'Ecommerce Product Page',
+    description: "This application allows the user to see the product detail from an Ecommerce. Functional Product Cart integrated. ",
+      image: '/images/ecommerce.png',
+      tags: ['React', 'Node'],
+    source: 'https://ecommerce-shoes.vercel.app/',
+    visit: 'https://github.com/NicoBlas/ecommerceShoes',
+    id: 0,
+  },
+  {
+    title: 'Space Tourism',
+    description:"In this project i learned to build a single-page App with React Router. ",
+    image: '/images/space.png',
+    tags: ['React', 'Node', "React Router"],
+    source: "https://space-tourism-two-pi.vercel.app/",
+    visit: "https://github.com/NicoBlas/spaceTourism",
+    id: 1,
+  },
+  {
+    title: 'IP Adress Tracker',
+    description: "This App let you input an IP and track its Geolocation and key Info. In this project i learned to consume 2 APIs with their libraries.",
+      image: '/images/iptrack.png',
+      tags: ['React', 'Node', "API"],
+    source: 'https://ip-tracker-two.vercel.app/',
+    visit: 'https://github.com/NicoBlas/ipTracker',
+    id: 2,
+  },
+  {
+    title: 'Bank Landing Page',
+    description: "While building this fully responsive Landing Page i learned to make a Contact Form using Formik.",
+    image: '/images/bank.png',
+    tags: ['React', "Formik", 'JavaScript', 'CSS'],
+    source: "https://easybank-lp-eta.vercel.app/",
+    visit: "https://github.com/NicoBlas/easybankLP",
+    id: 3,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: 'Started my journey', },
+  { year: 2018, text: 'Worked as a freelance developer', },
+  { year: 2019, text: 'Founded JavaScript Mastery', },
+  { year: 2020, text: 'Shared my projects with the world', },
+  { year: 2021, text: 'Started my own platform', },
+];
